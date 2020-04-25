553d37c8159121b5daa804191003720437843b36
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2020-04-25 10:16:49 +0200

    install packages

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-sudo pacman -S py3status xinput
+sudo pacman -S py3status xorg-xinput xf86-input-libinput wget unzip
+pacaur -S chromium-widevine
+sudo pacman -S  pulseaudio-bluetooth
+sudo pacman -S lightdm
+sudo systemctl enable lightdm.service
+sudo pacman -S lightdm lightdm-gtk-greeter
+sudo pacman -S xorg-server-xephyr
+sudo pacman -S nitrogen
+sudo pacman -R xscreensaver
+sudo pacman -S neofetch
+pacaur -S xfce4-notifyd
+pacaur -S xfce4-wavelan-plugin
