d1b2856265f879c1d6b14af3c5d72f45d110131f
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2020-12-13 09:21:25 +0100

    Create create_links.sh

---
+++ create_links.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.bash_profile ~/.bash_profile.bak
+
+ln -sf ~/dotfiles/.bashrc                ~/.bashrc
+ln -sf ~/dotfiles/.bashrc.aliases        ~/.bashrc.aliases
+ln -sf ~/dotfiles/.vimrc                 ~/.vimrc
