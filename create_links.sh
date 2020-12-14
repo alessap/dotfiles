cf001dec0b68c8d8c2701401c5f166b62e2282de
Alessandro Pirrotta <alessandro@pirrotta.xyz>
2020-12-14 20:07:08 +0100

    add powerline-shell in create links

--- create_links.sh
+++ create_links.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-mv ~/.bashrc ~/.bashrc.bak
-mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.bashrc                                       ~/.bashrc.bak
+mv ~/.bash_profile                                 ~/.bash_profile.bak
 
-ln -sf ~/dotfiles/.bashrc                ~/.bashrc
-ln -sf ~/dotfiles/.bashrc.aliases        ~/.bashrc.aliases
-ln -sf ~/dotfiles/.vimrc                 ~/.vimrc
+ln -sf ~/dotfiles/.bashrc                          ~/.bashrc
+ln -sf ~/dotfiles/.bashrc.aliases                  ~/.bashrc.aliases
+ln -sf ~/dotfiles/.vimrc                           ~/.vimrc
+ln -sf ~/dotfiles/.config/powerline-shell          ~/.config/powerline-shell
