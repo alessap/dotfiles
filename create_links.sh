b5ab9fa0662675d7434f4162e2b2d518bf1a149c
Alessandro Pirrotta <alessandro@pirrotta.xyz>
2021-04-04 12:27:50 +0200

    create links update

--- create_links.sh
+++ create_links.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-mv ~/.bashrc                                       ~/.bashrc.bak
-mv ~/.bash_profile                                 ~/.bash_profile.bak
+mv ~/.bashrc                                                         ~/.bashrc.bak
+mv ~/.bash_profile                                                   ~/.bash_profile.bak
 
-ln -sf ~/dotfiles/.bashrc                          ~/.bashrc
-ln -sf ~/dotfiles/.bashrc.aliases                  ~/.bashrc.aliases
-ln -sf ~/dotfiles/.vimrc                           ~/.vimrc
-ln -sf ~/dotfiles/.config/powerline-shell          ~/.config/powerline-shell
-ln -sf ~/dotfiles/.git-completion.bash             ~/.git-completion.bash
+ln -sf ~/dotfiles/.bashrc                                            ~/.bashrc
+ln -sf ~/dotfiles/.bashrc.aliases                                    ~/.bashrc.aliases
+ln -sf ~/dotfiles/.vimrc                                             ~/.vimrc
+ln -sf ~/dotfiles/.config/powerline-shell                            ~/.config/powerline-shell
+ln -sf ~/dotfiles/.git-completion.bash                               ~/.git-completion.bash
+ln -sf ~/dotfiles/arch-updates-notifier/updates-notifier.sh          ~/bin/updates-notifier.sh
+
+mkdir -p ~/.config/systemd/user/
+ln -sf ~/dotfiles/.config/systemd/user/updates-notifier.service      ~/.config/systemd/user/updates-notifier.service
+ln -sf ~/dotfiles/.config/systemd/user/updates-notifier.timer        ~/.config/systemd/user/updates-notifier.timer
+systemctl --machine=alessap@.host --user enable updates-notifier.timer
+systemctl --machine=alessap@.host --user start updates-notifier.timer
